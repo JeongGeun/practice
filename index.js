4c0c61b584d576650e7e7360517d283e199ca726
JeongGeun <40541480+JeongGeun@users.noreply.github.com>
2019-07-11 02:44:37 +0900

    third commit

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 var express = require('express');
 var app = express();
+var path=require('path')
 app.use(express.static('public'));
 app.get('/', function(req, res){
-    res.send('Hello home page');;
+    res.sendFile('plus.html',{root:path.join(__dirname,'./web')})
 });
-app.get('/route', function(req, res){
-    res.send('Hello Router, <img src="/route.png">')
-})
-app.get('/login', function(req, res){
-    res.send('<h1>Login please</h1>');
+app.get('/1', function(req, res){
+    res.sendFile('budongsan.html',{root:path.join(__dirname,'./web')})
 });
+app.get('/2', function(req, res){
+    res.sendFile('chaegwon.html',{root:path.join(__dirname,'./web')})
+});
+app.get('/3', function(req, res){
+    res.sendFile('dankikeumyung.html',{root:path.join(__dirname,'./web')})
+});
+app.get('/4', function(req, res){
+    res.sendFile('Gongmobudongsan.html',{root:path.join(__dirname,'./web')})
+});
+app.get('/5', function(req, res){
+    res.sendFile('Gongmotjasan.html',{root:path.join(__dirname,'./web')})
+});
+app.get('/6', function(req, res){
+    res.sendFile('honhabchaegwon.html',{root:path.join(__dirname,'./web')})
+});
+app.get('/7', function(req, res){
+    res.sendFile('honhapjoosick.html',{root:path.join(__dirname,'./web')})
+});
+app.get('/8', function(req, res){
+    res.sendFile('jaeganjeop.html',{root:path.join(__dirname,'./web')})
+});
+app.get('/9', function(req, res){
+    res.sendFile('joosick.html',{root:path.join(__dirname,'./web')})
+});
+app.get('/10', function(req, res){
+    res.sendFile('pasanghyung.html',{root:path.join(__dirname,'./web')})
+});
+app.get('/11', function(req, res){
+    res.sendFile('tjasan.html',{root:path.join(__dirname,'./web')})
+});
+
 app.listen(3000, function(){
     console.log('Conneted 3000 port!');
 });
